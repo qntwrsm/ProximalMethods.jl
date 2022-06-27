7acf953cecc1a57d06750fa10e2f7b5b74bec2d0
Quint Wiersma <qnt.wrsm@gmail.com>
2022-06-27 14:38:48 +0200

    Added docs to proximal operator for smooth functions, as well as new functionalities

--- src/proximal.jl
+++ src/proximal.jl
@@ -62,7 +62,7 @@ function block_soft_thresh!(y::AbstractVector, x::AbstractVector, λ::Real)
 	τ= max(one(λ) - λ*inv(norm(x) + eps(T)), zero(λ))
 
 	# Block soft thresholding
-	@. y= τ*x
+	y.= τ .* x
 
 	return nothing
 end
@@ -80,7 +80,7 @@ function block_soft_thresh!(x::AbstractVector, λ::Real)
 	τ= max(one(λ) - λ*inv(norm(x) + eps(T)), zero(λ))
 
 	# Block soft thresholding
-	@. x= τ*x
+	x.= τ .* x
 
 	return nothing
 end
@@ -116,7 +116,11 @@ proximal operator of a quadratic function with quadratic parameters `A` and
 linear parameters `b` using a factorization `fac` of ``I + λA``, overwriting
 `x`. See also `shrinkage`.
 """
-function shrinkage!(x::AbstractVector, λ::Real, fac::Factorization, b::AbstractVector)
+function shrinkage!(x::AbstractVector, 
+                    λ::Real, 
+                    fac::Factorization, 
+                    b::AbstractVector
+                    )
     # x - λb
     x.-= λ .* b 
     # (I + λA)⁻¹(x - λb)
@@ -141,7 +145,11 @@ linear parameters `b` using a factorization `fac` of ``I + λA``.
 #### Returns
   - `y::AbstractVector` : shrunken values
 """
-function shrinkage(x::AbstractVector, λ::Real, fac::Factorization, b::AbstractVector)
+function shrinkage( x::AbstractVector, 
+                    λ::Real, 
+                    fac::Factorization, 
+                    b::AbstractVector
+                    )
     y= similar(x)
     shrinkage!(y, λ, fac, b)
 
@@ -155,7 +163,11 @@ Compute the generalized shrinkage operator with scaling parameter `λ` at `x`,
 proximal operator of a quadratic function with quadratic parameters `A` and
 linear parameters `b`, overwriting `x`. See also `shrinkage`.
 """
-function shrinkage!(x::AbstractVector, λ::Real, A::AbstractMatrix, b::AbstractVector)
+function shrinkage!(x::AbstractVector, 
+                    λ::Real, 
+                    A::AbstractMatrix, 
+                    b::AbstractVector
+                    )
     # scaling I + λA
     S= λ .* A
     @inbounds @fastmath for i in axes(A,1)
@@ -186,14 +198,61 @@ linear parameters `b`.
 #### Returns
   - `y::AbstractVector` : shrunken values
 """
-function shrinkage(x::AbstractVector, λ::Real, A::AbstractMatrix, b::AbstractVector)
+function shrinkage( x::AbstractVector, 
+                    λ::Real, 
+                    A::AbstractMatrix, 
+                    b::AbstractVector
+                    )
     y= similar(x)
     shrinkage!(y, λ, A, b)
 
     return y
 end
 
-function smooth!(x::AbstractVector, λ::Real, f::Function, ∇f!::Function, x0::AbstractVector)
+"""
+    smooth(x, λ, f, ∇f!, y_prev)
+
+Compute the proximal operator of a general smooth function `f` with in-place
+gradient `∇f!` and scaling parameter `λ` at `x` using L-BFGS. Warm starting is
+accomodated by the use of `y_prev`, the previous solution.
+
+#### Arguments
+  - `x::AbstractVector`     : input
+  - `λ::Real`               : scaling parameter
+  - `f::Function`           : objective function
+  - `∇f!::Function`         : gradient
+  - `y_prev::AbstractVector`: previous output
+
+#### Returns
+  - `y::AbstractVector` : output
+"""
+function smooth(x::AbstractVector, 
+                λ::Real, 
+                f::Function, 
+                ∇f!::Function, 
+                y_prev::AbstractVector
+                )
+    y= similar(x)
+    smooth!(y, x, λ, f, ∇f!, y_prev)
+
+    return y
+end
+
+"""
+    smooth!(y, x, λ, f, ∇f!, y_prev)
+
+Compute the proximal operator of a general smooth function `f` with in-place
+gradient `∇f!` and scaling parameter `λ` at `x` using L-BFGS, storing the
+results in `y`. See also `smooth!`. Warm starting is accomodated by the use of
+`y_prev`, the previous solution.
+"""
+function smooth!(   y::AbstractVector, 
+                    x::AbstractVector, 
+                    λ::Real, 
+                    f::Function, 
+                    ∇f!::Function, 
+                    y_prev::AbstractVector
+                    )
     # adjust objective function and gradient
     # f(y) + (1/2λ)‖y - x‖₂² 
     g(y::AbstractVector)= f(y) + inv(λ + λ) * ( sum(abs2, y) + sum(abs2, x) - 2 * dot(y, x) )
@@ -206,10 +265,46 @@ function smooth!(x::AbstractVector, λ::Real, f::Function, ∇f!::Function, x0::
                                                 end
     
     # solve using L-BFGS
-    res= optimize(g, ∇g!, x0, LBFGS(), Optim.Options(g_tol = 1e-4))
-    x.= Optim.minimizer(res)
+    res= optimize(g, ∇g!, y_prev, LBFGS(), Optim.Options(g_tol = 1e-4))
+    y.= Optim.minimizer(res)
+
     # store current estimate to warm start next iteration
-    x0.= x
+    y_prev.= y
+
+    return nothing
+end
+
+"""
+    smooth!(x, λ, f, ∇f!, y_prev)
+
+Compute the proximal operator of a general smooth function `f` with in-place
+gradient `∇f!` and scaling parameter `λ` at `x` using L-BFGS, overwriting `x`.
+See also `smooth!`. Warm starting is accomodated by the use of `y_prev`, the
+previous solution.
+"""
+function smooth!(   x::AbstractVector, 
+                    λ::Real, 
+                    f::Function, 
+                    ∇f!::Function, 
+                    y_prev::AbstractVector
+                    )
+    # adjust objective function and gradient
+    # f(y) + (1/2λ)‖y - x‖₂² 
+    g(y::AbstractVector)= f(y) + inv(λ + λ) * ( sum(abs2, y) + sum(abs2, x) - 2 * dot(y, x) )
+    # ∇f(y) + (1/λ)(y - x)
+    ∇g!(∇g::AbstractVector, y::AbstractVector)= begin
+                                                    # gradient    
+                                                    ∇f!(∇g, y)
+                                                    # quadratic term
+                                                    ∇g.+= inv(λ) .* (y .- x)
+                                                end
+    
+    # solve using L-BFGS
+    res= optimize(g, ∇g!, y_prev, LBFGS(), Optim.Options(g_tol = 1e-4))
+    x.= Optim.minimizer(res)
+
+    # store current estimate to warm start next iteration
+    y_prev.= x
 
     return nothing
 end
